d0559e8feaa1bbd329871e932bded26eb90c5c18
noahburren <noah.burren@bluewin.ch>
2025-10-24 10:16:22 +0200

    remove unused import

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { PerfumeListComponent } from '../perfumes/perfume-list.component';
-import {AppComponent} from './app.component';
 
 export const routes: Routes = [
     { path: 'perfumes', component: PerfumeListComponent },
